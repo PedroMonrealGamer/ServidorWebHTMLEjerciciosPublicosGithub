be2dbc381a36b8be60ca27491d88d5181224c380
PedroMonreal <pedromonrealsanchez@gmail.com>
2024-02-06 21:11:17 +0100

    index

--- js.js
+++ js.js
@@ -1,13 +1,15 @@
-var webs = ['CarruselFotos','ProyectoWebResponsiveMobileFirst(BreakPoint)'];
+var url = ['CarruselFotos','ProyectoWebResponsiveMobileFirst(BreakPoint)'];
+var web = ['CarruselFotos','ProyectoWebResponsiveMobileFirst(BreakPoint)'];
+
 crearbotones()
 
 function crearbotones() {
-    for (var i = 0; i < webs.length; i++) {
+    for (var i = 0; i < url.length; i++) {
         var boton = document.createElement("a");
         boton.id = "boton" + i;
         boton.className = "boton";
-        boton.href = webs[i]; 
-        boton.innerHTML = webs[i];
+        boton.href = url[i]; 
+        boton.innerHTML = web[i];
         var divbotones = document.getElementById("botones");
         divbotones.appendChild(boton);
     }
