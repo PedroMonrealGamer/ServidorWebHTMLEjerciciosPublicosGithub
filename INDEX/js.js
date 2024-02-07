e7f707232371e2259678fd586c31bcfacba9f34d
PedroMonreal <pedromonrealsanchez@gmail.com>
2024-02-07 11:10:35 +0100

    Mejora de web index

--- INDEX/js.js
+++ INDEX/js.js
@@ -1,8 +1,9 @@
 var url = ['CarruselFotos', 'ProyectoWebResponsiveMobileFirst(BreakPoint)', 'WebCadenaJSON'];
 var web = ['Carrusel Fotos', 'Proyecto WebResponsive Mobile First (BreakPoint)', 'Web con cadena JSON'];
-
+var fotoscarrusel = ['INDEX//fotos/1.png', 'INDEX//fotos/2.png','INDEX//fotos/3.png'];
+var fotoactual = 0;
 crearBotones();
-
+crearcarrusel();
 function crearBotones() {
     var divBotones = document.getElementById("botones");
     for (var i = 0; i < url.length; i++) {
@@ -12,5 +13,38 @@ function crearBotones() {
         boton.href = url[i];
         boton.innerHTML = web[i];
         divBotones.appendChild(boton);
-    } 
+        
+    }
 }
+function crearcarrusel() {
+    for (var i = 0; i < fotoscarrusel.length; i++) {
+        var enlace = document.createElement("a");
+        enlace.href = url[i];
+        var foto = document.createElement("img");
+        foto.src = fotoscarrusel[i];
+        foto.id = "foto" + i;
+        foto.classList.add("oculto");
+        enlace.appendChild(foto);
+        var divcarrusel = document.getElementById("contenedorcarrusel");
+        divcarrusel.appendChild(enlace);
+        var descripcion = document.getElementById("descripcion").textContent =web[fotoactual]
+
+    }
+    var fotoprimera = document.getElementById("foto0");
+    fotoprimera.classList.remove("oculto");
+}
+
+function pasarfotos() {
+    var fotoocultar = document.getElementById("foto" + fotoactual);
+    fotoocultar.classList.add("oculto");
+    fotoactual++;
+    if (fotoactual === fotoscarrusel.length) {
+        fotoactual = 0;
+    }
+    var fotoactualmostrar = document.getElementById("foto" + fotoactual);
+    fotoactualmostrar.classList.remove("oculto");
+    var descripcion = document.getElementById("descripcion").textContent =web[fotoactual]
+}
+//Cambiar las fotos automáticamente
+  setInterval(function() {
+    pasarfotos();}, 3000); 
